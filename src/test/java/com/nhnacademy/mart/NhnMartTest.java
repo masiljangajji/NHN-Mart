95862be0b1961e33b55df8698c8a2c7e1636b857
masiljangajji <hjk172262@gmail.com>
2023-09-02 22:43:44 +0900

    test: pickFoodTest 추가 및 기존 테스트 refactoring (stream 변경)

--- src/test/java/com/nhnacademy/mart/NhnMartTest.java
+++ src/test/java/com/nhnacademy/mart/NhnMartTest.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.mart;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -47,9 +49,11 @@ class NhnMartTest {
         int count2 = 0;
 
         Stream<Food> stream = nhnMart.getFoodStand().getFoods().stream();
+        Stream<Food> stream2 = nhnMart.getFoodStand().getFoods().stream();
+
 
         count1 = (int) stream.filter(x -> x.getName().equals("양파")).count();
-        count2 = (int) stream.filter(x -> x.getName().equals("계란")).count();
+        count2 = (int) stream2.filter(x -> x.getName().equals("계란")).count();
 
         Assertions.assertEquals(count1, 2);
         Assertions.assertEquals(count2, 5);
@@ -63,11 +67,12 @@ class NhnMartTest {
         int count2 = 0;
 
         Stream<Food> stream = nhnMart.getFoodStand().getFoods().stream();
+        Stream<Food> stream2 = nhnMart.getFoodStand().getFoods().stream();
 
 
         count1 = (int) stream.filter(x -> x.getPrice() == 1000)
                 .filter(x -> x.getName().equals("양파")).count();
-        count2 = (int) stream.filter(x -> x.getPrice() == 2000)
+        count2 = (int) stream2.filter(x -> x.getPrice() == 2000)
                 .filter(x -> x.getName().equals("사과")).count();
 
         Assertions.assertEquals(count1, 2);
@@ -81,13 +86,48 @@ class NhnMartTest {
         int count1 = 0;
         int count2 = 0;
 
+
         Stream<Food> stream = nhnMart.getFoodStand().getFoods().stream();
+        Stream<Food> stream2 = nhnMart.getFoodStand().getFoods().stream();
+
 
         count1 = (int) stream.filter(x -> x.getPrice() == 1000).count();
-        count2 = (int) stream.filter(x -> x.getPrice() == 2000).count();
+        count2 = (int) stream2.filter(x -> x.getPrice() == 2000).count();
 
         Assertions.assertEquals(count1, 2);
         Assertions.assertEquals(count2, 20);
+    }
+
+    @Test
+    @DisplayName("pickFood postCondition Test 물품과 개수가 식품 매대와 충분한 경우")
+    void pickFoodTest() {
+
+        customer = new Customer(buyList);
+        customer.bring(nhnMart.provideBasket());
+
+        customer.pickFoods(nhnMart.getFoodStand()); // 내가 고른 물품들이 그대로 basket에
+
+        Iterator<BuyList.Item> iterator = buyList.getItems().iterator(); // 바이리스트
+
+        // 바이 리스트랑 똑같으면 됨
+
+        int count = 0;
+
+        ArrayList<Food> myBasket = customer.getBasket().getBasketList();
+
+
+        while (iterator.hasNext()) {
+            BuyList.Item item = iterator.next();
+
+            for (int i = 0; i < myBasket.size(); i++) {// 바이리스트에 있는 이름과 같으면 count
+                if (myBasket.get(i).getName().equals(item.getName())) {
+                    count++; // 이름이 같은거 전부 체크
+                }
+            }
+        }
+
+        Assertions.assertEquals(count, myBasket.size());
+
     }
 
 
