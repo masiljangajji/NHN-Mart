47c694db79c7f493152e7e8dfeacd4b8f0a6495a
masiljangajji <hjk172262@gmail.com>
2023-09-02 23:37:06 +0900

    feat: logging 추가 및 Foodstand.findFood 예외처리

--- src/main/java/com/nhnacademy/mart/FoodStand.java
+++ src/main/java/com/nhnacademy/mart/FoodStand.java
@@ -4,8 +4,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FoodStand {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(FoodStand.class);
+
+    public ArrayList<Food> getFoods() {
+        return foods;
+    }
 
     private final ArrayList<Food> foods = new ArrayList<>();
 
@@ -18,7 +27,7 @@ public class FoodStand {
     public ArrayList<Food> findFood(HashMap<String, Integer> hashMap) {
 
 
-        Iterator<Map.Entry<String, Integer>> iterator = hashMap.entrySet().iterator();
+        Iterator<Map.Entry<String, Integer>> iterator = hashMap.entrySet().iterator();// 바이리스트
         ArrayList<Food> result = new ArrayList<>();
 
 
@@ -29,16 +38,29 @@ public class FoodStand {
 
             ArrayList<Integer> idxList = new ArrayList<>();
 
+            boolean flag = true;
+
             for (int j = 0; j < foods.size(); j++) { // 사과 3개를 전부 픽해서 옴
                 if (foods.get(j).getName().equals(m.getKey())) {
                     result.add(new Food(m.getKey(), foods.get(j).getPrice())); // 장바구니에 , 사과 | 사과의 가격  담아줄꺼임 ,
                     idxList.add(j);
                 }
 
-                // TODO 수량보다 적으면 Exception 터치기
                 if (idxList.size() == m.getValue()) {
-                    break;
+                    break; // 같으면 다 산거임
                 }
+            }
+
+            if (idxList.isEmpty()) {
+                logger.warn("매대에 없는 물품 구입");
+                throw new IllegalArgumentException(m.getKey() + "은(는) 매대에 없는 물품입니다");
+            }
+
+            //  내가 살 수량보다 적은거 예외처리
+            if (idxList.size() < m.getValue()) {
+                // TODO 로그 찍기
+                logger.warn("구입품목의 수량이 매대에 무품수량보다 큼");
+                throw new IllegalArgumentException(m.getKey() + "은(는) 매대에 수량이 부족합니다.");
             }
 
             for (int j = 0; j < idxList.size(); j++) {
@@ -53,5 +75,4 @@ public class FoodStand {
     }
 
 
-    // TODO 장바구니에 담은 Food 삭제 구현
 }
