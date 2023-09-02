c2a9fab091ae01ea722406f7316df4b9a3d1e712
masiljangajji <hjk172262@gmail.com>
2023-09-02 17:40:11 +0900

    feat: FoodStand findFood 추가

--- src/main/java/com/nhnacademy/mart/FoodStand.java
+++ src/main/java/com/nhnacademy/mart/FoodStand.java
@@ -12,6 +12,21 @@ public class FoodStand {
         foods.add(food);
     }
 
+    public Food findFood(String foodName) {
+
+        for (int i = 0; i < foods.size(); i++) {
+
+            Food food = foods.get(i);
+
+            if (foodName.equals(food.getName())) {
+                return food;
+            }
+        }
+
+        throw new IllegalArgumentException(foodName + "은(는) 식품 매대에 존재하지 않습니다. ");
+
+    }
+
 
     // TODO 장바구니에 담은 Food 삭제 구현
 }
