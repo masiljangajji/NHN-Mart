2f97fd5fb2dd3bf193a08373aef43a37602dd4cb
masiljangajji <hjk172262@gmail.com>
2023-09-02 19:14:59 +0900

    refactor: FoodStand findFood , Custormer pickFoods(BuyList 압축)

--- src/main/java/com/nhnacademy/mart/Customer.java
+++ src/main/java/com/nhnacademy/mart/Customer.java
@@ -1,5 +1,8 @@
 package com.nhnacademy.mart;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Customer {
 
     // 고객 구매 목록
@@ -23,17 +26,26 @@ public class Customer {
         // basket에 음식 넣으럮임 , add Food 할꺼임
 
         // foodStand 에서 food를 찾을 꺼야
-        for (int i = 0; i < buyList.getItems().size(); i++) {
 
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        for (int i = 0; i < buyList.getItems().size(); i++) {
 
             // food 찾아서 , 가격 넣는거 까지만
             BuyList.Item item = buyList.getItems().get(i); // 내가 살 물건과 , 그 숫자가 있음
 
-            int price = foodStand.findFood(item.getName(), item.getAmount());
 
-            basket.add(new Food(item.getName(),
-                    price)); // 내가 살 물건과 , 그 가격을 장바구니에 넣어야 함 ,  ||||  사과 1000원 , 5개 사기 -> 사과 5000 으로 정리됨  , 품목 - 총 가격
+            if (hashMap.get(item.getName()).equals(null)) {
+                hashMap.put(item.getName(), item.getAmount());
+            } else {
+                hashMap.put(item.getName(), hashMap.get(item.getName()) + item.getAmount());
+            }
+        }
 
+        ArrayList<Food> result = foodStand.findFood(hashMap);
+
+        for (int i = 0; i < result.size(); i++) {
+            basket.add(result.get(i));
         }
     }
 
@@ -42,3 +54,8 @@ public class Customer {
 
 
 }
+
+
+
+
+
--- src/main/java/com/nhnacademy/mart/FoodStand.java
+++ src/main/java/com/nhnacademy/mart/FoodStand.java
@@ -1,6 +1,9 @@
 package com.nhnacademy.mart;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class FoodStand {
 
@@ -12,31 +15,30 @@ public class FoodStand {
         foods.add(food);
     }
 
-    public int findFood(String foodName, int amount) {
+    public ArrayList<Food> findFood(HashMap<String, Integer> hashMap) {
 
-        ArrayList<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < foods.size(); i++) {
+        Iterator<Map.Entry<String, Integer>> iterator = hashMap.entrySet().iterator();
+        ArrayList<Food> result = new ArrayList<>();
 
-            Food food = foods.get(i);
 
-            if (foodName.equals(food.getName())) { // foodStand 에 있는 물건 , 수량만큼 지워줘야 함
-                list.add(i); // idx 저장  , 후에 지워주면 됨
+        while (iterator.hasNext()) { // 사과 3개 , 바나나 5개 , 원래는 사과 사과 바나나 바나나 바나나 바나나 사과 바나나
+
+            Map.Entry<String, Integer> m = iterator.next();
+
+            int amount = m.getValue();
+
+            for (int i = 0; i < amount; i++) { // 사과 3개
+
+                // TODO 수량보다 적으면 Exception 터치기
+
+                int idx = foods.indexOf(m.getKey()); // 사과 , 사과 없으면 -1 반환됨 이걸로 Exception 터치기
+
+                int foodCost = foods.get(idx).getPrice(); // 사과 가격
+
+                result.add(new Food(m.getKey(), foodCost)); // 장바구니에 , 사과 | 사과의 가격  담아줄꺼임 ,
+                foods.remove(idx); // 식품 매대에서 물건 삭제
             }
-        }
-
-        // TODO 수량보다 적으면 Exception 터치기 list.size()< amount
-
-
-        int result = 0;
-
-        // remove 시키고 , 남은 용량만큼 넣어주면 된다 .
-        for (int i = 0; i < list.size(); i++) {
-
-            int numidx = list.get(i);
-
-            result += foods.get(numidx).getPrice();
-            foods.remove(list.get(i));
         }
 
         return result;
